c000a047e70fa0f701d2856342c5c21376b267b6
Howard Osborne <howardosborneconsulting@gmail.com>
2020-07-11 16:50:13 +0100

    cols

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from setuptools import find_packages, setup
 setup(
     name='locust-scripting-challenge',
     version='0.1.0',
-    packages=['locust-scripting-challenge'],
+    packages=find_packages(),
+    include_package_data=True,
     description='A challenge to help learn how to write a performance test script',
     url='https://github.com/howardosborne/locust-scripting-challenge.git',
     author='Howard Osborne',
     author_email='howardosborneconsulting@gmail.com',
     license='MIT',
-    include_package_data=True,
     zip_safe=False,
     install_requires=[
         'flask',
-        'lxml'
+        'lxml',
     ],
 )
