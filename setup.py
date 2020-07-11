9e5e76d15fe0a521b3f7945d109d15b09dec3888
Howard Osborne <howardosborneconsulting@gmail.com>
2020-07-11 11:57:21 +0100

    typo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='locust-scripting-challenge',
     version='0.1.0',
-    packages=['locust-script-challenge'],
+    packages=['locust-scripting-challenge'],
     description='A challenge to help learn how to write a performance test script',
     url='https://github.com/howardosborne/locust-scripting-challenge',
     author='Howard Osborne',
