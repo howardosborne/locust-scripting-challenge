303f175fef6f83dc8d99e547ebc16be76d0c3701
Howard Osborne <howardosborneconsulting@gmail.com>
2020-07-11 11:25:20 +0100

    first commit

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='locust_scripting_challenge',
+    name='locust-scripting-challenge',
     version='0.1.0',
-    packages=['locust_script_challenge'],
+    packages=['locust-script-challenge'],
     description='A challenge to help learn how to write a performance test script',
     url='https://github.com/howardosborne/locust_scripting_challenge',
     author='Howard Osborne',
