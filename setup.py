199f02d80c14cd23fe086b961fc0c1bc42209dec
Howard Osborne <howardosborneconsulting@gmail.com>
2020-07-11 11:33:08 +0100

    changed setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     packages=['locust-script-challenge'],
     description='A challenge to help learn how to write a performance test script',
-    url='https://github.com/howardosborne/locust_scripting_challenge',
+    url='https://github.com/howardosborne/locust-scripting-challenge',
     author='Howard Osborne',
     author_email='howardosborneconsulting@gmail.com',
     license='MIT',
